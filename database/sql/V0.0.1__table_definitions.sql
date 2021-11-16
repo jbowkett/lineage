64f1e78bf278d02e01d178b3358b1ae13781cc7b
jbowkett <james@bowkett.info>
2021-11-16 07:02:24 +0000

    CHORE: Some spring magic to allow neo components to startup properly BUGFIX: updated the table name when adding the participant static data

--- database/sql/V0.0.1__table_definitions.sql
+++ database/sql/V0.0.1__table_definitions.sql
@@ -19,12 +19,12 @@ CREATE TABLE IF NOT EXISTS lineage.participant(
     participant_type varchar
 );
 
-insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (1, '{"rowId":1,"tableName":"lineage.participant","parents":[]}', 'Credit Suisse', 'brokerage');
-insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (2, '{"rowId":2,"tableName":"lineage.participant","parents":[]}', 'James Bowkett', 'individual');
-insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (3, '{"rowId":3,"tableName":"lineage.participant","parents":[]}', 'Bernie Madoff', 'individual');
-insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (4, '{"rowId":4,"tableName":"lineage.participant","parents":[]}', 'Citigroup, Inc.', 'brokerage');
-insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (5, '{"rowId":5,"tableName":"lineage.participant","parents":[]}', 'Long Term Capital Management', 'fund');
-insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (6, '{"rowId":6,"tableName":"lineage.participant","parents":[]}', 'Knight Capital', 'fund');
+insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (1, '{"rowId":1,"tableName":"participant","parents":[]}', 'Credit Suisse', 'brokerage');
+insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (2, '{"rowId":2,"tableName":"participant","parents":[]}', 'James Bowkett', 'individual');
+insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (3, '{"rowId":3,"tableName":"participant","parents":[]}', 'Bernie Madoff', 'individual');
+insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (4, '{"rowId":4,"tableName":"participant","parents":[]}', 'Citigroup, Inc.', 'brokerage');
+insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (5, '{"rowId":5,"tableName":"participant","parents":[]}', 'Long Term Capital Management', 'fund');
+insert into lineage.participant(participant_id, lineage, participant_name, participant_type) VALUES (6, '{"rowId":6,"tableName":"participant","parents":[]}', 'Knight Capital', 'fund');
 
 CREATE TABLE IF NOT EXISTS lineage.participant_summary(
     row_id varchar primary key,
